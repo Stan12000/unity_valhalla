43c11cb732b3c3025ca166f6698d56289ff95010
ricky0624 <ricky23750719@gmail.com>
2017-02-02 19:31:25 +0800

    Ver1
    2017.2.2

---
+++ RPG_Game/Assets/Scripts/ProcBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcBase : MonoBehaviour {
+
+	public virtual void onEnter(){};
+	public virtual void onLeave(){};
+}
