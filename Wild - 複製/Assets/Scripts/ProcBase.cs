6789c48ee0b22bd2af94aa2529f56d0bc87602ba
Stan12000 <ra72636@gmail.com>
2017-02-03 17:26:39 +0800

    V2.2
    test

--- Wild - 複製/Assets/Scripts/ProcBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ProcBase : MonoBehaviour {
-
-	public virtual void onEnter(){};
-	public virtual void onLeave(){};
-}
