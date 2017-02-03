e690f6ea3c447041a2006a36dec5d24dbafea2d0
duckQQ <ukpiyo@gmail.com>
2017-02-03 17:11:45 +0800

    Map
    test

---
+++ Wild - 複製/Assets/Scripts/ProcManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcManager : MonoBehaviour {
+	
+	public enum ProcID
+	{
+		None = 0,
+		Login,
+		CreateChar,
+		ForeWord,
+		Main,
+	}
+
+	public ProcManager.ProcID curProcID;
+    ProcBase [] procs
+
+	public void SwitchProc(ProcID procID)
+	{
+		//destory curProcID
+		ProcBase v = new ProcBase ();
+		v.onLeave();
+		//change curProcID -> procID
+		curProcID = procID;
+
+
+	}
+
+
+}
---
+++ Wild - 複製/Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+	
+	public enum UIID
+	{
+		Login = 0,
+		Loading,
+		Job,
+		CharName,
+		ForeWord,
+		Main,
+	}
+	
+}
