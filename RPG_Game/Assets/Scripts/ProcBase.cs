d0f273483479defa20638206c91b374ab77c6534
Stan12000 <ra72636@gmail.com>
2017-02-06 20:28:35 +0800

    應該行了

---
+++ RPG_Game/Assets/Scripts/ProcBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcBase : MonoBehaviour {
+	
+	public virtual void OnEnter(){}
+	public virtual void OnLeave(){}
+}
+
---
+++ RPG_Game/Assets/Scripts/ProcLogin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcLogin : ProcBase {
+	
+	public override void OnEnter ()
+	{
+		Debug.Log ("I am ProcLogin");
+	}
+}
